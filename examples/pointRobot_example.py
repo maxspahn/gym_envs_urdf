051f8842aca4af29fc102fd6e91ab901eb5d5e0d
Max Spahn <m.spahn@tudelft.nl>
2021-11-15 08:44:45 +0100

    Udptase example pointRobotUrdf

--- examples/pointRobot_example.py
+++ examples/pointRobot_example.py
@@ -6,15 +6,20 @@ import numpy as np
 
 
 def main():
-    env = gym.make('pointRobotUrdf-vel-v0', dt=0.01, render=True)
-    defaultAction = np.array([0.1, 0.3, 1.0])
+    env = gym.make('pointRobotUrdf-acc-v0', dt=0.05, render=True)
+    defaultAction = np.array([0.0, 0.0])
     n_episodes = 1
     n_steps = 100000
     cumReward = 0.0
+    pos0 = np.array([1.0, 0.1])
+    vel0 = np.array([1.0, 0.0])
     for e in range(n_episodes):
-        ob = env.reset()
+        ob = env.reset(pos=pos0, vel=vel0)
+        env.setWalls(limits=[[-3, -2], [3, 2]])
         print("Starting episode")
+        t = 0
         for i in range(n_steps):
+            t += env.dt()
             action = defaultAction
             ob, reward, done, info = env.step(action)
             cumReward += reward
