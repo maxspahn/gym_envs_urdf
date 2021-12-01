1e7862469ab5f469e839e00e33a1d3f5183ac82d
Max <m.spahn@tudelft.nl>
2021-12-01 14:43:35 +0100

    Adapts mobileRobot to fit the abstractRobot class

--- mobileReacher/resources/mobileRobot.py
+++ mobileReacher/resources/mobileRobot.py
@@ -1,130 +1,41 @@
 import pybullet as p
-import pybullet_data
-import gym
 import os
-import math
-from urdfpy import URDF
 import numpy as np
 
+from urdfCommon.abstractRobot import AbstractRobot
 
-class MobileRobot:
+
+class MobileRobot(AbstractRobot):
     def __init__(self, gripper=False):
         self._gripper = gripper
         if gripper:
-            self.f_name = os.path.join(os.path.dirname(__file__), 'mobilePandaWithGripper.urdf')
-            self._n = 12
-            self.robot_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 13, 14]
-            self.control_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 13, 14]
+            fileName = os.path.join(os.path.dirname(__file__), 'mobilePandaWithGripper.urdf')
+            n = 12
         else:
-            self.f_name = os.path.join(os.path.dirname(__file__), 'mobilePanda.urdf')
-            self._n = 10
+            fileName = os.path.join(os.path.dirname(__file__), 'mobilePanda.urdf')
+            n = 10
+        super().__init__(n, fileName)
+
+    def setJointIndices(self):
+        if self._gripper:
             self.robot_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10]
-            self.control_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10]
-        self.readLimits()
+            self.urdf_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10]
+        else:
+            self.robot_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10]
+            self.urdf_joints = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10]
 
-    def addObstacle(self, pos, filename):
-        f_name = os.path.join(os.path.dirname(__file__), "cylinder.urdf")
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.loadURDF(
-            filename,
-            basePosition=pos
-        )
-
-    def n(self):
-        return self._n
-
-    def reset(self, pos=np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.501, 0.0, 1.8675, 0.0]), vel=None):
-        self.robot = p.loadURDF(fileName=self.f_name,
-                              basePosition=[0, 0, 0.0])
-        # Joint indices as found by p.getJointInfo()
-        for i in range(self._n):
-            p.setJointMotorControl2(self.robot, self.control_joints[i],
-                                        controlMode=p.POSITION_CONTROL,
-                                        targetPosition=pos[i])
-        print("Bringing to initial position..")
-        pre_steps = 100
-        for i in range(pre_steps):
-            p.stepSimulation()
-        print("Reached initial position")
+    def reset(self, pos=None, vel=None):
         self._vels_int = vel
+        return super().reset(pos=pos, vel=vel)
 
-    def getLimits(self):
-        return (self._limitPos_j, self._limitVel_j, self._limitTor_j, self._limitAcc_j)
-
-    def readLimits(self):
-        robot = URDF.load(self.f_name)
-        self._limitPos_j = np.zeros((2, self._n))
-        self._limitVel_j = np.zeros((2, self._n))
-        self._limitAcc_j = np.zeros((2, self._n))
-        self._limitTor_j = np.zeros((2, self._n))
-        for i, j in enumerate(self.control_joints):
-            joint = robot.joints[j]
-            print("joint : ", joint.name)
-            self._limitPos_j[0, i] = joint.limit.lower
-            self._limitPos_j[1, i] = joint.limit.upper
-            self._limitVel_j[0, i] = -joint.limit.velocity
-            self._limitVel_j[1, i] = joint.limit.velocity
-            self._limitTor_j[0, i] = -joint.limit.effort
-            self._limitTor_j[1, i] = joint.limit.effort
+    def setAccLimits(self):
         accLimit = np.array([1.0, 1.0, 1.0, 15.0, 7.5, 10.0, 12.5, 15.0, 20.0, 20.0, 1.0, 1.0])
         self._limitAcc_j[0, :] = -accLimit[0:self._n]
         self._limitAcc_j[1, :] = accLimit[0:self._n]
 
-    def getTorqueSpaces(self):
-        xu = np.concatenate((self._limitPos_j[1, :], self._limitVel_j[1, :]))
-        xl = np.concatenate((self._limitPos_j[0, :], self._limitVel_j[0, :]))
-        uu = self._limitTor_j[1, :]
-        ul = self._limitTor_j[0, :]
-        ospace = gym.spaces.Box(low=xl, high=xu, dtype=np.float64)
-        aspace = gym.spaces.Box(low=ul, high=uu, dtype=np.float64)
-        return(ospace, aspace)
-
-    def getAccSpaces(self):
-        xu = np.concatenate((self._limitPos_j[1, :], self._limitVel_j[1, :]))
-        xl = np.concatenate((self._limitPos_j[0, :], self._limitVel_j[0, :]))
-        uu = self._limitAcc_j[1, :]
-        ul = self._limitAcc_j[0, :]
-        ospace = gym.spaces.Box(low=xl, high=xu, dtype=np.float64)
-        aspace = gym.spaces.Box(low=ul, high=uu, dtype=np.float64)
-        return(ospace, aspace)
-
-    def getVelSpaces(self):
-        xu = self._limitPos_j[1, :]
-        xl = self._limitPos_j[0, :]
-        uu = self._limitVel_j[1, :]
-        ul = self._limitVel_j[0, :]
-        ospace = gym.spaces.Box(low=xl, high=xu, dtype=np.float64)
-        aspace = gym.spaces.Box(low=ul, high=uu, dtype=np.float64)
-        return(ospace, aspace)
-
-    def disableVelocityControl(self):
-        self._friction = 0
-        for i in range(self._n):
-            p.setJointMotorControl2(
-                self.robot,
-                jointIndex=self.robot_joints[i],
-                controlMode=p.VELOCITY_CONTROL,
-                force=self._friction
-            )
-
-    def get_ids(self):
-        return self.robot
-
-    def apply_torque_action(self, torques):
-        for i in range(self._n):
-            p.setJointMotorControl2(self.robot, self.robot_joints[i],
-                                        controlMode=p.TORQUE_CONTROL,
-                                        force=torques[i])
-
     def apply_acc_action(self, accs, dt):
         self._vels_int += dt * accs
         self.apply_vel_action(self._vels_int)
-
-    def apply_vel_action(self, vels):
-        for i in range(self._n):
-            p.setJointMotorControl2(self.robot, self.robot_joints[i],
-                                        controlMode=p.VELOCITY_CONTROL,
-                                        targetVelocity=vels[i])
 
     def moveGripper(self, gripperVel):
         # TODO Why can't I use velocity control here..
@@ -132,20 +43,3 @@ class MobileRobot:
             p.setJointMotorControl2(self.robot, self.gripper_joints[i],
                                         controlMode=p.POSITION_CONTROL,
                                         targetPosition=gripperVel)
-
-    def get_observation(self):
-        # Get Joint Configurations
-        joint_pos_list = []
-        joint_vel_list = []
-        for i in range(self._n):
-            pos, vel, _, _= p.getJointState(self.robot, self.robot_joints[i])
-            joint_pos_list.append(pos)
-            joint_vel_list.append(vel)
-        joint_pos = np.array(joint_pos_list)
-        joint_vel = np.array(joint_vel_list)
-
-        # Concatenate position, orientation, velocity
-        return np.concatenate((joint_pos, joint_vel))
-
-if __name__ == "__main__":
-    mr = MobileRobot()
