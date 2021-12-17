fd5084c1975be8990b6ab4e464937dab03ca2b0b
Max <m.spahn@tudelft.nl>
2021-12-17 09:05:09 +0100

    Adds boxerRobot.

---
+++ boxerRobot/envs/boxerRobotEnv.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from boxerRobot.resources.boxerRobot import BoxerRobot
+from urdfCommon.urdfEnv import UrdfEnv
+
+
+class BoxerRobotEnv(UrdfEnv):
+
+    def __init__(self, render=False, dt=0.01):
+        super().__init__(BoxerRobot(), render=render, dt=dt)
+        self.setSpaces()
+
+    def checkInitialState(self, pos, vel):
+        if not isinstance(pos, np.ndarray) or not pos.size == self.robot.n()+1:
+            pos = np.zeros(self.robot.n()+1)
+        if not isinstance(vel, np.ndarray) or not vel.size == self.robot.n():
+            vel = np.zeros(self.robot.n())
+        return pos, vel
