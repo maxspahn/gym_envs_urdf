ddabb6b55fcdd4738da46e9c88f9641a13736d45
Max <m.spahn@tudelft.nl>
2022-02-14 17:12:07 +0100

    Reset nLinkUrdf example.

--- examples/nLinkUrdf_reacher.py
+++ examples/nLinkUrdf_reacher.py
@@ -4,24 +4,20 @@ import numpy as np
 
 
 def main():
-    n = 4
+    n = 3
     #env = gym.make('nLink-reacher-acc-v0', n=n, dt=0.01)
-    env = gym.make('nLink-urdf-reacher-vel-v0', n=n, dt=0.01, render=True)
+    env = gym.make('nLink-urdf-reacher-acc-v0', n=n, dt=0.01, render=True)
     defaultAction = np.ones(n) * 0.1
     n_episodes = 1
-    n_steps = 1
+    n_steps = 1000
     cumReward = 0.0
-    pos0 = np.array([1.0, 0.0, 0.0, 0.1])
-    vel0 = np.array([0.0, 0.0, 0.0, 0.0])
     for e in range(n_episodes):
-        ob = env.reset(pos=pos0, vel=vel0)
-        print(ob['x'])
+        ob = env.reset()
         print("Starting episode")
         for i in range(n_steps):
             action = env.action_space.sample()
             action = defaultAction
             ob, reward, done, info = env.step(action)
-            print(ob['x'])
             cumReward += reward
 
 
