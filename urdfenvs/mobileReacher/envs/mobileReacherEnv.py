aa52a39083fbf2d0dd5f9b937e1851d5115c4a9c
Max <m.spahn@tudelft.nl>
2022-02-23 09:40:50 +0100

    Fixes initial state for mobile panda.

--- urdfenvs/mobileReacher/envs/mobileReacherEnv.py
+++ urdfenvs/mobileReacher/envs/mobileReacherEnv.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import numpy as np
 
 from urdfenvs.mobileReacher.resources.mobileRobot import MobileRobot
 from urdfenvs.urdfCommon.urdfEnv import UrdfEnv
@@ -10,6 +11,19 @@ class MobileReacherEnv(UrdfEnv):
         super().__init__(MobileRobot(gripper=gripper), render=render, dt=dt)
         self._n = self.robot.n()
         self.setSpaces()
+
+    def checkInitialState(self, pos, vel):
+        if not isinstance(pos, np.ndarray) or not pos.size == self.robot.n():
+            pos = np.zeros(self.robot.n())
+            pos[6] = -1.501
+            pos[8] = 1.8675
+            pos[9] = np.pi/4
+            if self.robot.n() > 10:
+                pos[10] = 0.02
+                pos[11] = 0.02
+        if not isinstance(vel, np.ndarray) or not vel.size == self.robot.n():
+            vel = np.zeros(self.robot.n())
+        return pos, vel
 
     @abstractmethod
     def setSpaces(self):
