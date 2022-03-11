a2dffce7ee68e9c48146395eb694e78cabb75c9a
Max <m.spahn@tudelft.nl>
2022-03-11 11:18:40 +0100

    Cleans plane.py.

--- urdfenvs/urdfCommon/plane.py
+++ urdfenvs/urdfCommon/plane.py
@@ -1,11 +1,10 @@
 import pybullet as p
-import pybullet_data
 import os
 
 
 class Plane:
     def __init__(self):
         f_name = os.path.join(os.path.dirname(__file__), 'plane.urdf')
-        plane = p.loadURDF(f_name)
+        p.loadURDF(f_name)
 
 
