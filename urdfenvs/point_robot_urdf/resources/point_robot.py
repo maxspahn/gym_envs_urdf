7b6da0390344a8f76b16bd7065063fb2b2b21de0
Skylove Qiu <z.qiu@student.tudelft.nl>
2022-05-08 03:09:02 +0200

    auto point robot

--- urdfenvs/point_robot_urdf/resources/point_robot.py
+++ urdfenvs/point_robot_urdf/resources/point_robot.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-
+from urdfpy import URDF
 from urdfenvs.urdfCommon.holonomic_robot import HolonomicRobot
 
 
@@ -11,8 +11,13 @@ class PointRobot(HolonomicRobot):
         super().__init__(n, urdf_file)
 
     def set_joint_indices(self):
-        self._robot_joints = [0, 1, 2]
-        self._urdf_joints = [0, 1, 2]
+        self._joint_names = ["mobile_joint_x","mobile_joint_y","mobile_joint_theta"]
+        robot = URDF.load(self._urdf_file) 
+        self._urdf_joints = [] 
+        for i, joint in enumerate(robot.joints): 
+            if joint.name in self._joint_names: 
+                self._urdf_joints.append(i) 
+        self.get_indexed_joint_info()
 
     def set_acceleration_limits(self):
         acc_limit = np.ones(self._n)
