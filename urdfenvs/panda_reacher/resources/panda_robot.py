20d3e26da68d4cd69e9a2c02b396256f089d13b5
Skylove Qiu <z.qiu@student.tudelft.nl>
2022-05-08 02:05:47 +0200

    panda auto

--- urdfenvs/panda_reacher/resources/panda_robot.py
+++ urdfenvs/panda_reacher/resources/panda_robot.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-
+from urdfpy import URDF
 from urdfenvs.urdfCommon.holonomic_robot import HolonomicRobot
 
 
@@ -22,11 +22,30 @@ class PandaRobot(HolonomicRobot):
 
     def set_joint_indices(self):
         if self._gripper:
-            self._robot_joints = [1, 2, 3, 4, 5, 6, 7, 9, 10]
-            self._urdf_joints = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+            panda_finger_joint_names = ["panda_finger_joint1","panda_finger_joint2"] 
+            panda_joint_names = ["panda_joint"+str(i) for i in range(1,8)]
+            self._joint_names = ( 
+                panda_finger_joint_names+
+                panda_joint_names 
+            )
         else:
-            self._robot_joints = [1, 2, 3, 4, 5, 6, 7]
-            self._urdf_joints = [1, 2, 3, 4, 5, 6, 7]
+            panda_joint_names = ["panda_joint"+str(i) for i in range(1,8)]
+            self._joint_names = ( 
+                panda_joint_names 
+            )
+        robot = URDF.load(self._urdf_file)
+        self._urdf_joints = [] 
+        for i, joint in enumerate(robot.joints):
+            #print(str(i) + ":  " + joint.name) 
+            if joint.name in self._joint_names: 
+                self._urdf_joints.append(i) 
+        self.get_indexed_joint_info()
+        # if self._gripper:
+        #     self._robot_joints = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+        #     self._urdf_joints = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+        # else:
+        #     self._robot_joints = [1, 2, 3, 4, 5, 6, 7]
+        #     self._urdf_joints = [1, 2, 3, 4, 5, 6, 7]
 
     def set_acceleration_limits(self):
         acc_limit = np.array(
