b55b47b1bee60714b08fe0d2fbc4c6e58190a36c
Chadi Salmi <c.salmi@tudelft.nl>
2022-10-05 15:37:28 +0200

    Almost working velocity tests, only differential drive has an assertion error #120

--- tests/test_vel_envs.py
+++ tests/test_vel_envs.py
@@ -1,79 +1,88 @@
 import gym
 import numpy as np
 import pytest
-import urdfenvs.albert_reacher
-import urdfenvs.point_robot_urdf
-import urdfenvs.panda_reacher
-import urdfenvs.n_link_urdf_reacher
-import urdfenvs.dual_arm
-import urdfenvs.tiago_reacher
-import urdfenvs.boxer_robot
+import urdfenvs.urdf_common
 
 @pytest.fixture
 def pointRobotEnv():
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
     init_pos = np.array([0.0, -1.0, 0.0])
     init_vel = np.array([-1.0, 0.0, 0.0])
-    env = gym.make("pointRobotUrdf-vel-v0", render=False, dt=0.01)
+    robot = GenericUrdfReacher(urdf="pointRobot.urdf", mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def pandaRobotEnv():
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
     init_pos = np.array([0.0, 0.0, 0.0, -1.875, 0.0, 1.5, 0.0])
     init_vel = np.array([0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 0.0])
-    env = gym.make("panda-reacher-vel-v0", dt=0.01, render=False, gripper=False)
-    ob = env.reset(pos=init_pos, vel=init_vel)
+    robot = GenericUrdfReacher(urdf="panda.urdf", mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
+    _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def nLinkRobotEnv():
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
     n = 1
     init_pos = np.array([0.0])
     init_vel = np.array([0.0])
-    env = gym.make("nLink-urdf-reacher-vel-v0", n=n, dt=0.01, render=False)
+    robot = GenericUrdfReacher(urdf=f"nlink_{n}.urdf", mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def boxerRobotEnv():
+    from urdfenvs.robots.boxer import BoxerRobot
     init_pos = np.array([0.0, 0.0, 0.0])
     init_vel = np.array([0.0, 0.0])
-    env = gym.make("boxer-robot-vel-v0", dt=0.01, render=False)
+    robot = BoxerRobot(mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def tiagoReacherEnv():
+    from urdfenvs.robots.tiago import TiagoRobot
     init_pos = np.zeros(20)
     init_pos[3] = 0.1
     init_vel = np.zeros(19)
-    env = gym.make("tiago-reacher-vel-v0", dt=0.01, render=False)
+    robot = TiagoRobot(mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def albertReacherEnv():
+    from urdfenvs.robots.albert import AlbertRobot
     init_pos = np.zeros(10)
     init_pos[6] = -1.501
     init_pos[8] = 1.8675
     init_pos[9] = -np.pi/4
     init_vel = np.zeros(9)
-    env = gym.make("albert-reacher-vel-v0", dt=0.01, render=False)
+    robot = AlbertRobot(mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def priusEnv():
-    import urdfenvs.prius
+    from urdfenvs.robots.prius import Prius
     initPos = np.zeros(3)
     initVel = np.zeros(2)
-    env = gym.make("prius-vel-v0", dt=0.01, render=False)
-    ob = env.reset(pos=initPos, vel=initVel)
+    robot = Prius(mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
+    _ = env.reset(pos=initPos, vel=initVel)
     return (env, initPos, initVel)
 
 @pytest.fixture
 def dualArmEnv():
-    env = gym.make("dual-arm-vel-v0", dt=0.01, render=False)
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
+    robot = GenericUrdfReacher(urdf="dual_arm.urdf", mode="vel")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     init_pos = np.zeros(env.n())
     init_vel = np.zeros(env.n())
     _ = env.reset(pos=init_pos, vel=init_vel)
@@ -95,42 +104,42 @@ def test_all(allEnvs):
     for env in allEnvs:
         ob = env[0].reset(pos=env[1], vel=env[2])
         action = np.random.random(env[0].n())
-        np.testing.assert_array_almost_equal(ob['joint_state']['position'], env[1], decimal=2)
+        np.testing.assert_array_almost_equal(ob['robot_0']['joint_state']['position'], env[1], decimal=2)
         ob, _, _, _ = env[0].step(action)
-        assert isinstance(ob, dict)
-        assert isinstance(ob['joint_state']['position'], np.ndarray)
-        assert isinstance(ob['joint_state']['velocity'], np.ndarray)
-        assert ob['joint_state']['position'].size == env[0].n()
-        np.testing.assert_array_almost_equal(ob['joint_state']['velocity'], action, decimal=2)
+        assert isinstance(ob['robot_0'], dict)
+        assert isinstance(ob['robot_0']['joint_state']['position'], np.ndarray)
+        assert isinstance(ob['robot_0']['joint_state']['velocity'], np.ndarray)
+        assert ob['robot_0']['joint_state']['position'].size == env[0].n()
+        np.testing.assert_array_almost_equal(ob['robot_0']['joint_state']['velocity'], action, decimal=2)
 
 def test_allDifferentialDrive(allDifferentialDriveEnvs):
     for env in allDifferentialDriveEnvs:
         ob = env[0].reset(pos=env[1], vel=env[2])
         action = np.random.random(env[0].n()) * 0.1
-        np.testing.assert_array_almost_equal(ob['joint_state']['position'], env[1], decimal=2)
+        np.testing.assert_array_almost_equal(ob['robot_0']['joint_state']['position'], env[1], decimal=2)
         ob, _, _, _ = env[0].step(action)
-        assert isinstance(ob, dict)
-        assert isinstance(ob['joint_state']['position'], np.ndarray)
-        assert isinstance(ob['joint_state']['velocity'], np.ndarray)
-        assert isinstance(ob['joint_state']['forward_velocity'], np.ndarray)
-        assert ob['joint_state']['position'].size == env[0].n() + 1
-        assert ob['joint_state']['velocity'].size == env[0].n() + 1
-        np.testing.assert_array_almost_equal(ob['joint_state']['forward_velocity'], action[0], decimal=2)
-        np.testing.assert_array_almost_equal(ob['joint_state']['velocity'][3:], action[2:], decimal=2)
+        assert isinstance(ob['robot_0'], dict)
+        assert isinstance(ob['robot_0']['joint_state']['position'], np.ndarray)
+        assert isinstance(ob['robot_0']['joint_state']['velocity'], np.ndarray)
+        assert isinstance(ob['robot_0']['joint_state']['forward_velocity'], np.ndarray)
+        assert ob['robot_0']['joint_state']['position'].size == env[0].n() + 1
+        assert ob['robot_0']['joint_state']['velocity'].size == env[0].n() + 1
+        np.testing.assert_array_almost_equal(ob['robot_0']['joint_state']['forward_velocity'], action[0], decimal=2)
+        np.testing.assert_array_almost_equal(ob['robot_0']['joint_state']['velocity'][3:], action[2:], decimal=2)
 
 def test_allBicycleModel(allBicycleModelEnvs):
     for env in allBicycleModelEnvs:
         ob = env[0].reset(pos=env[1], vel=env[2])
         action = np.random.random(env[0].n()) * 0.1
-        np.testing.assert_array_almost_equal(ob['x'], env[1], decimal=2)
+        np.testing.assert_array_almost_equal(ob['robot_0']['x'], env[1], decimal=2)
         ob, _, _, _ = env[0].step(action)
-        assert isinstance(ob, dict)
-        assert isinstance(ob['x'], np.ndarray)
-        assert isinstance(ob['xdot'], np.ndarray)
-        assert isinstance(ob['vel'], np.ndarray)
-        assert ob['x'].size == env[0].n() + 1
-        assert ob['xdot'].size == env[0].n() + 1
-        assert ob['vel'].size == 2
-        np.testing.assert_array_almost_equal(ob['vel'][0:1], action[0:1], decimal=2)
-        np.testing.assert_array_almost_equal(ob['xdot'][3:], action[2:], decimal=2)
+        assert isinstance(ob['robot_0'], dict)
+        assert isinstance(ob['robot_0']['x'], np.ndarray)
+        assert isinstance(ob['robot_0']['xdot'], np.ndarray)
+        assert isinstance(ob['robot_0']['vel'], np.ndarray)
+        assert ob['robot_0']['x'].size == env[0].n() + 1
+        assert ob['robot_0']['xdot'].size == env[0].n() + 1
+        assert ob['robot_0']['vel'].size == 2
+        np.testing.assert_array_almost_equal(ob['robot_0']['vel'][0:1], action[0:1], decimal=2)
+        np.testing.assert_array_almost_equal(ob['robot_0']['xdot'][3:], action[2:], decimal=2)
 
