85aa117473dbfa4e683072bb07ef4ee8acfc115b
Max <m.spahn@tudelft.nl>
2022-10-10 16:02:22 +0200

    Corrects acceleration test.

--- tests/test_acc_envs.py
+++ tests/test_acc_envs.py
@@ -4,36 +4,39 @@ import pytest
 
 @pytest.fixture
 def pointRobotEnv():
-    import urdfenvs.point_robot_urdf
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
     init_pos = np.array([0.0, -1.0, 0.0])
     init_vel = np.array([-1.0, 0.0, 0.0])
-    env = gym.make("pointRobotUrdf-acc-v0", render=False, dt=0.01)
+    robot = GenericUrdfReacher(urdf="pointRobot.urdf", mode="acc")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def pandaRobotEnv():
-    import urdfenvs.panda_reacher
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
     init_pos = np.array([0.0, 0.0, 0.0, -1.875, 0.0, 1.5, 0.0])
     init_vel = np.array([0.0, 0.0, 1.0, 0.0, 0.0, 0.2, 0.0])
-    env = gym.make("panda-reacher-acc-v0", dt=0.01, render=False, gripper=False)
+    robot = GenericUrdfReacher(urdf="panda.urdf", mode="acc")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def nLinkRobotEnv():
-    n = 1
-    import urdfenvs.n_link_urdf_reacher
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
     init_pos = np.array([0.0])
     init_vel = np.array([0.0])
-    env = gym.make("nLink-urdf-reacher-acc-v0", n=n, dt=0.01, render=False)
+    robot = GenericUrdfReacher(urdf="nlink_1.urdf", mode="acc")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     _ = env.reset(pos=init_pos, vel=init_vel)
     return (env, init_pos, init_vel)
 
 @pytest.fixture
 def dualArmEnv():
-    import urdfenvs.dual_arm
-    env = gym.make("dual-arm-acc-v0", dt=0.01, render=False)
+    from urdfenvs.robots.generic_urdf import GenericUrdfReacher
+    robot = GenericUrdfReacher(urdf="dual_arm.urdf", mode="acc")
+    env = gym.make("urdf-env-v0", robots=[robot], render=False, dt=0.01)
     init_pos = np.zeros(env.n())
     init_vel = np.zeros(env.n())
     _ = env.reset(pos=init_pos, vel=init_vel)
@@ -47,10 +50,10 @@ def test_all(allEnvs):
     for env in allEnvs:
         ob = env[0].reset(pos=env[1], vel=env[2])
         action = np.random.random(env[0].n())
-        np.testing.assert_array_almost_equal(ob['joint_state']['position'], env[1], decimal=2)
+        np.testing.assert_array_almost_equal(ob['robot_0']['joint_state']['position'], env[1], decimal=2)
         ob, _, _, _ = env[0].step(action)
         assert isinstance(ob, dict)
-        assert isinstance(ob['joint_state']['position'], np.ndarray)
-        assert isinstance(ob['joint_state']['velocity'], np.ndarray)
-        assert ob['joint_state']['position'].size == env[0].n()
+        assert isinstance(ob['robot_0']['joint_state']['position'], np.ndarray)
+        assert isinstance(ob['robot_0']['joint_state']['velocity'], np.ndarray)
+        assert ob['robot_0']['joint_state']['position'].size == env[0].n()
 
