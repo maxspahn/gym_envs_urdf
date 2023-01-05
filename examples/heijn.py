015b9be92fe0006215adbc924623ca7cd901c277
Max <m.spahn@tudelft.nl>
2023-01-05 15:56:21 +0100

    Adds collision links to heijn robot.

--- examples/heijn.py
+++ examples/heijn.py
@@ -10,7 +10,7 @@ def run_point_robot(n_steps=1000, render=False, goal=True, obstacles=True):
         "urdf-env-v0",
         dt=0.01, robots=robots, render=render
     )
-    action = np.array([0.1, 0.0, 0.0])
+    action = np.array([0.1, 0.0, 0.01])
     pos0 = np.array([1.0, 0.1, 0.0])
     vel0 = np.array([1.0, 0.0, 0.0])
     ob = env.reset(pos=pos0, vel=vel0)
@@ -20,13 +20,11 @@ def run_point_robot(n_steps=1000, render=False, goal=True, obstacles=True):
         from examples.scene_objects.obstacles import (
             sphereObst1,
             sphereObst2,
-            urdfObst1,
             dynamicSphereObst3,
         )
 
         env.add_obstacle(sphereObst1)
         env.add_obstacle(sphereObst2)
-        env.add_obstacle(urdfObst1)
         env.add_obstacle(dynamicSphereObst3)
     if goal:
         from examples.scene_objects.goal import splineGoal
