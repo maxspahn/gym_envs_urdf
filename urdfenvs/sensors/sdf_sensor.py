eeb1be3db662eccb9a33ffb1908f14e0c4c8e949
Max <m.spahn@tudelft.nl>
2023-07-27 16:18:51 +0200

    ft[sensor]: Using the numpified version of distance calculation for obstacles to increase the computation of the sdf sensor.

--- urdfenvs/sensors/sdf_sensor.py
+++ urdfenvs/sensors/sdf_sensor.py
@@ -6,16 +6,18 @@ import gymnasium as gym
 
 from urdfenvs.sensors.sensor import Sensor
 
+
 class LinkIdNotFoundError(Exception):
     pass
 
 
 class SDFSensor(Sensor):
-    def __init__(self,
-                 limits: np.ndarray =  np.array([[-1, -1], [-1, 1], [-1, 1]]),
-                 resolution: np.ndarray = np.array([10, 10, 10], dtype=int),
-                 interval: int = -1,
-                ):
+    def __init__(
+        self,
+        limits: np.ndarray = np.array([[-1, -1], [-1, 1], [-1, 1]]),
+        resolution: np.ndarray = np.array([10, 10, 10], dtype=int),
+        interval: int = -1,
+    ):
         super().__init__("SDFSensor")
         self._resolution = resolution
         self._limits = limits
@@ -25,7 +27,9 @@ class SDFSensor(Sensor):
         x_values = np.linspace(limits[0][0], limits[0][1], resolution[0])
         y_values = np.linspace(limits[1][0], limits[1][1], resolution[1])
         z_values = np.linspace(limits[2][0], limits[2][1], resolution[2])
-        self._mesh = np.stack(np.meshgrid(x_values, y_values, z_values, indexing="ij"), axis=-1)
+        self._mesh = np.stack(
+            np.meshgrid(x_values, y_values, z_values, indexing="ij"), axis=-1
+        )
         self._sdf = np.zeros(shape=self._mesh.shape[0:3])
 
     def mesh(self) -> np.ndarray:
@@ -47,18 +51,26 @@ class SDFSensor(Sensor):
 
     def sense(self, robot, obstacles: dict, goals: dict, t: float):
         self._call_counter += 1
-        if self._computed and (self._interval < 0 or self._call_counter % self._interval != 0):
+        if self._computed and (
+            self._interval < 0 or self._call_counter % self._interval != 0
+        ):
             return self._sdf
-        t0 = perf_counter()
-        for i in range(self._resolution[0]):
-            for j in range(self._resolution[1]):
-                for k in range(self._resolution[2]):
-                    pos = self._mesh[i, j, k]
-                    distance = np.min(np.array([obstacle.distance(pos) for obstacle in list(obstacles.values())]))
-                    self._sdf[i, j, k] = np.maximum(0.0, distance)
-        t1 = perf_counter()
-        print(f"Computed SDF in {t1-t0}s")
+        start_time = perf_counter()
+        mesh_flat = self._mesh.reshape((-1, 3))
+        self._sdf = np.maximum(
+            np.min(
+                np.array(
+                    [
+                        obstacle.distance(mesh_flat)
+                        for obstacle in list(obstacles.values())
+                    ]
+                ),
+                axis=0,
+            ),
+            0.0,
+        ).reshape(self._resolution)
+        end_time = perf_counter()
+
+        print(f"Computed SDF in {end_time-start_time} s")
         self._computed = True
         return self._sdf
-
-
