c54b228dddfccb6fe2228eb41010e3b154e0cd65
Max <m.spahn@tudelft.nl>
2023-12-19 21:35:53 +0100

    ft[mujoco]: Adds example to compare mujoco and pybullet directly.

--- examples/panda_mujoco.py
+++ examples/panda_mujoco.py
@@ -1,19 +1,46 @@
 import numpy as np
+import sys
 import gymnasium as gym
 from urdfenvs.urdf_common.generic_mujoco_env import GenericMujocoEnv
+from urdfenvs.urdf_common.generic_mujoco_robot import GenericMujocoRobot
+from urdfenvs.robots.generic_urdf import GenericUrdfReacher
+from urdfenvs.urdf_common.urdf_env import UrdfEnv
+from urdfenvs.scene_examples.obstacles import sphereObst1, sphereObst2, wall_obstacles, cylinder_obstacle
 
-env: GenericMujocoEnv = gym.make("generic-mujoco-env-v0", xml_file='panda_scene_with_box.xml', render_mode='human')
+
+def run_panda(n_steps: int = 1000, render: bool = True, physics_engine: str = 'bullet'):
+    obstacles = [sphereObst1, sphereObst2, cylinder_obstacle] + wall_obstacles
+    if physics_engine == 'mujoco':
+        robots  = [
+            GenericMujocoRobot(xml_file='panda_scene.xml'),
+        ]
+        env = GenericMujocoEnv(robots, obstacles, render=True)
+    if physics_engine == 'bullet':
+        robots = [
+            GenericUrdfReacher(urdf="panda.urdf", mode="vel"),
+        ]
+        env= UrdfEnv(
+            dt=0.01, robots=robots,
+            render=True,
+            observation_checking=False,
+        )
+        for obstacle in obstacles:
+            env.add_obstacle(obstacle)
+        env.set_spaces()
 
 
-observation, info = env.reset(seed=42)
-pos0=np.array([0,0,0,-1.57079,0,1.57079,-0.7853,0.04,0.04])
-env.reset_model(pos=pos0)
+    #observation, info = env.reset(seed=42)
+    pos0=np.array([0,0,0,-1.57079,0,1.57079,-0.7853,0.04,0.04])
+    env.reset(pos=pos0)
 
-action_mag = np.array([0.3, -0.2, 0.3, -0.15, 0.2, -0.01, 0.35, 0.01])
-N = 300
-t = 0.0
-for _ in range(N):
-    t += env.dt
-    action = action_mag * np.cos(t)
-    observation, *_ = env.step(action)
-env.close()
+    action_mag = np.array([0.3, -0.2, 0.3, -0.15, 0.2, -0.01, 0.35, 0.01])
+    N = 1000
+    t = 0.0
+    for _ in range(N):
+        t += env.dt
+        action = action_mag * np.cos(t)
+        observation, *_ = env.step(action)
+    env.close()
+
+if __name__ == "__main__":
+    run_panda(render=True, physics_engine=sys.argv[1])
